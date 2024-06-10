98c5bcd8ae2c06f5437437807a2d9f0d2c51522d
Praval Pratap Singh <113175059+Pravalpratap@users.noreply.github.com>
2024-06-10 15:30:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let btn=document.getElementById("enterBtn");
+let tag=document.getElementById("status");
+
+btn.addEventListener("click",()=>{
+	tag.innerHTML="<h1> Entered Metaverse</h1>"
+})
